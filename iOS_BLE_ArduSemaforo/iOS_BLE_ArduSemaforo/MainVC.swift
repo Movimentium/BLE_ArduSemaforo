0648870406d54262bacf65fa61fff73500d2b626
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2020-08-23 00:16:25 +0200

    First Commit

---
+++ iOS_BLE_ArduSemaforo/iOS_BLE_ArduSemaforo/MainVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainVC.swift
+//  iOS_BLE_ArduSemaforo
+//
+//  Created by Miguel on 23/08/2020.
+//  Copyright © 2020 Miguel Gallego Martín. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
